dd1d6a537967d0a4938c485250fea323c7e121cf
koteitan <ukaibutton@mac.com>
2017-03-21 02:41:35 +0900

    Comments are added.

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 window.onload=function(){ //entry point
 }
 
+/* pseudo-asm code
+  'nop'       ... do nothing
+  'A'--'Z'    ... pointer A--Z
+  'p',A,X,Y   ... pointer at (X,Y) in 2 dimensional array A
+  'mov',X,Y   ... copy X into Y
+  '-',X,Y     ... X minus Y
+  '+',X,Y     ... X plus  Y
+  '*',X,Y     ... X times Y
+  '|',X,Y     ... X or    Y
+  '&',X,Y     ... X and   Y
+  'jmp',X     ... jump to line X
+  'cnj',V,X,Y ... jump to line X if V otherwise jump to line Y
+  'lpu',A,X,Y ... push loop stack with address A from X to Y
+  'lpo'       ... pop loop stack
+  'prn',V     ... print V
+*/
 var list=[
   [0,'A=9:'                             ,['mov','A',9]],
-  [1,'dim B[Åá,Åá],C[Åá,Åá],D[Åá]<br>'  ,['nop','','']],
+  [1,'dim B[Åá,Åá],C[Åá,Åá],D[Åá]<br>'  ,['nop']],
   [2,'for E=0 to 9<br>'                 ,['lpu','E',0,9,1]],
   [3,' for F=0 to A<br>'                ,['lpu','F',0,'A']],
   [4,'  B[1,F]=1:'                      ,['mov','p','B',1,'F',1]], 
